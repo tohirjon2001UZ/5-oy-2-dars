f505be6766cda63be2e16812a46059eea1743501
tohirjon <tohirjon>
2025-09-13 15:59:28 +0500

    initial editor

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const elContainer = document.getElementById("container");
 const elCard = document.getElementById("cardTemplate");
 const elSkeleton = document.getElementById("skeletonTemplate");
 const elError = document.getElementById("error");
+const elForm = document.getElementById("form");
+const elAlert = document.getElementById("successAlert");
+const elChangeAlert = document.getElementById("changeAlert");
+const elAddBtn = document.getElementById("addBtn");
+const elEditBtn = document.getElementById("editBtn");
 
 let skeleton = getSkletionCount();
 for (let i = 0; i < skeleton; i++) {
   elContainer.append(elSkeleton.cloneNode(true).content);
 }
+
+let editId = null;
 
 function init() {
   fetch("https://json-api.uz/api/project/fn43/cars")
@@ -42,6 +49,69 @@ function deleteEl(id) {
     .then((res) => {})
     .finally(() => {});
 }
+function getByid(id) {
+  fetch(`https://json-api.uz/api/project/fn43/cars/${id}`)
+    .then((res) => res.json())
+    .then((res) => {
+      fill(res);
+    })
+    .finally(() => {});
+}
+
+function addEl(newEl) {
+  elContainer.innerHTML = "";
+  let skeleton = getSkletionCount();
+  for (let i = 0; i < skeleton; i++) {
+    elContainer.append(elSkeleton.cloneNode(true).content);
+  }
+
+  fetch(`https://json-api.uz/api/project/fn43/cars`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newEl),
+  })
+    .then((res) => {
+      elAlert.classList.remove("hidden");
+      setTimeout(() => {
+        elAlert.classList.add("hidden");
+      }, 2000);
+      init();
+    })
+    .finally(() => {});
+}
+
+function editEl(editedEl) {
+  elContainer.innerHTML = "";
+  let skeleton = getSkletionCount();
+  for (let i = 0; i < skeleton; i++) {
+    elContainer.append(elSkeleton.cloneNode(true).content);
+  }
+
+  fetch(`https://json-api.uz/api/project/fn43/cars/${editedEl.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(editedEl),
+  })
+    .then((res) => {
+      elChangeAlert.classList.remove("hidden");
+      setTimeout(() => {
+        elChangeAlert.classList.add("hidden");
+      }, 2000);
+      init();
+    })
+    .finally(() => {});
+}
+
+function fill(obj) {
+  elForm.name.value = obj.name;
+  elForm.description.value = obj.description;
+  elForm.category.value = obj.category;
+  elForm.price.value = obj.price;
+}
 
 init();
 
@@ -53,11 +123,13 @@ function info(card) {
     const elDescription = clone.querySelector("p");
     const elCategory = clone.querySelector("mark");
     const elDeleteBtn = clone.querySelector(".delete-btn");
+    const elEditeBtn = clone.querySelector(".edit-btn");
     const elInfoBtn = clone.querySelector(".info-btn");
     elTitle.innerText = car.name;
     elDescription.innerText = car.description;
     elCategory.innerText = car.category;
     elDeleteBtn.id = car.id;
+    elEditeBtn.id = car.id;
     elInfoBtn.id = car.id;
     elInfoBtn.addEventListener("click", () => {
       window.location.href = `about.html?id=${car.id}`;
@@ -78,10 +150,42 @@ function getSkletionCount() {
 }
 
 document.addEventListener("click", (evt) => {
+  // Delet
   if (evt.target.classList.contains("delete-btn")) {
     deleteEl(evt.target.id);
   }
+  // Info
   if (evt.target.classList.contains("info-btn")) {
     window.location.href = `about.html?id=${evt.target.id}`;
   }
+  // Edit
+  if (evt.target.classList.contains("edit-btn")) {
+    getByid(evt.target.id);
+    editId = evt.target.id;
+    elAddBtn.classList.add("hidden");
+    elEditBtn.classList.remove("hidden");
+
+    
+  }
+});
+
+elForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const formData = new FormData(elForm);
+  const result = {};
+  formData.forEach((velue, key) => {
+    result[key] = velue;
+  });
+
+  if (evt.submitter.id === "addB") {
+    addEl(result);
+  }
+  if (evt.submitter.id === "editBtn") {
+    if (editId) {
+      result.id = editId;
+      editEl(result);
+      editId = null;
+    }
+  }
+  elForm.reset();
 });
